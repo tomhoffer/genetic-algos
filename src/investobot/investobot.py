91134b5e91e57bb67b78df09bbfe5c6b0634d52d
Tomas Hoffer <tomas.hoffer@gmail.com>
2022-11-21 21:34:12 +0100

    Investobot: Implement crossover

--- src/investobot/investobot.py
+++ src/investobot/investobot.py
@@ -9,11 +9,26 @@ import pandas as pd
 from dotenv import load_dotenv
 from numpy.random import default_rng
 
+from src.generic.crossover import Crossover
 from src.generic.model import Solution
 from src.generic.mutation import Mutation
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
+
+
+def post_process_amounts(amounts: np.ndarray):
+    # Ensure budget is fully used and not exceeded
+    amounts_diff = amounts.sum() - float(os.environ.get("BUDGET"))
+    amounts_diff_per_ticker = abs(amounts_diff / len(amounts))
+    updated_amounts = amounts
+    if amounts_diff > 0:
+        updated_amounts = amounts - amounts_diff_per_ticker
+
+    if amounts_diff < 0:
+        updated_amounts = amounts + amounts_diff_per_ticker
+
+    return updated_amounts
 
 
 class InvestobotSolution(Solution):
@@ -93,20 +108,30 @@ def mutate(chromosome: np.ndarray) -> np.ndarray:
     mutated_timestamps = Mutation.mutate_real_gaussian(timestamps, min=0, max=int(os.environ.get("END_TIMESTAMP")))
 
     # Ensure budget is fully used and not exceeded
-    mutated_amounts_diff = mutated_amounts.sum() - float(os.environ.get("BUDGET"))
-    mutated_amounts_diff_per_ticker = abs(mutated_amounts_diff / len(tickers))
-
-    if mutated_amounts_diff > 0:
-        mutated_amounts = mutated_amounts - mutated_amounts_diff_per_ticker
-
-    if mutated_amounts_diff < 0:
-        mutated_amounts = mutated_amounts + mutated_amounts_diff_per_ticker
+    mutated_amounts = post_process_amounts(mutated_amounts)
 
     return np.stack((mutated_tickers, mutated_amounts, mutated_timestamps), axis=-1)
 
 
-def crossover():
-    yield
+def crossover(parent1: InvestobotSolution, parent2: InvestobotSolution):
+    res1, res2 = Crossover.two_point(parent1, parent2)
+
+    res1_tickers: np.ndarray = InvestobotSolution.parse_chromosome_tickers(res1.chromosome)
+    res1_amounts: np.ndarray = InvestobotSolution.parse_chromosome_amounts(res1.chromosome)
+    res1_timestamps: np.ndarray = InvestobotSolution.parse_chromosome_timestamps(res1.chromosome)
+
+    res2_tickers: np.ndarray = InvestobotSolution.parse_chromosome_tickers(res2.chromosome)
+    res2_amounts: np.ndarray = InvestobotSolution.parse_chromosome_amounts(res2.chromosome)
+    res2_timestamps: np.ndarray = InvestobotSolution.parse_chromosome_timestamps(res2.chromosome)
+
+    # Ensure budget is fully used and not exceeded
+    res1_updated_amounts = post_process_amounts(res1_amounts)
+    res2_updated_amounts = post_process_amounts(res2_amounts)
+
+    return (
+        InvestobotSolution(chromosome=np.stack((res1_tickers, res1_updated_amounts, res1_timestamps), axis=-1)),
+        InvestobotSolution(chromosome=np.stack((res2_tickers, res2_updated_amounts, res2_timestamps), axis=-1)),
+    )
 
 
 if __name__ == "__main__":
--- test/investobot/test_investobot.py
+++ test/investobot/test_investobot.py
@@ -1,6 +1,6 @@
 from typing import List
 import numpy as np
-from src.investobot.investobot import initial_population_generator, InvestobotSolution, mutate
+from src.investobot.investobot import initial_population_generator, InvestobotSolution, mutate, crossover
 from conftest import mockenv
 
 
@@ -47,3 +47,35 @@ def test_mutate(mocker):
     assert np.all((after_timestamps >= 0) & (after_timestamps <= 1000))
     assert np.all((after_tickers >= 0) & (after_tickers <= 3))
     np.testing.assert_almost_equal(after_amounts.sum(), 1000, decimal=5)
+
+
+@mockenv(BUDGET="1000")
+def test_crossover():
+    parent1 = InvestobotSolution(np.asarray([
+        [0, 250.0, 1],
+        [1, 250.0, 250],
+        [2, 250.0, 500],
+        [3, 250.0, 1000],
+    ]))
+    parent2 = InvestobotSolution(np.asarray([
+        [1, 100.0, 1],
+        [1, 400.0, 100],
+        [3, 250.0, 500],
+        [2, 250.0, 700],
+    ]))
+    res1, res2 = crossover(parent1, parent2)
+
+    assert res1.chromosome.shape == res2.chromosome.shape == parent1.chromosome.shape == parent2.chromosome.shape
+
+    unique_parent_tickers = set(parent1.get_chromosome_tickers() + parent2.get_chromosome_tickers())
+    unique_result_tickers = set(res1.get_chromosome_tickers() + res2.get_chromosome_tickers())
+    unique_parent_timestamps = set(parent1.get_chromosome_timestamps() + parent2.get_chromosome_timestamps())
+    unique_result_timestamps = set(res1.get_chromosome_timestamps() + res2.get_chromosome_timestamps())
+
+    # Tickers and timestamps come from parent chromosomes
+    assert unique_parent_timestamps == unique_result_timestamps
+    assert unique_parent_tickers == unique_result_tickers
+
+    # Amounts are adjusted to match the budget
+    np.testing.assert_almost_equal(res1.get_chromosome_amounts().sum(), 1000, decimal=5)
+    np.testing.assert_almost_equal(res2.get_chromosome_amounts().sum(), 1000, decimal=5)
