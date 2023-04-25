Tradingbot: Fix tests after adjusting selling profit calculation

test/tradingbot/test_tradingbot.py
@@ -48,7 +48,24 @@ def test_tradingbotsolution_sell(mocker):
     s.sell(datetime=timestamp_sold)
 
     assert len(s.bought_positions) == 9
-    assert s.account_balance == 9
+    assert s.account_balance == 1 + 9
+
+
+@mockenv(TRADED_TICKER_NAME="AAPL", BUDGET="20", TRADE_SIZE="10")
+def test_buy_sell(mocker):
+    df = load_ticker_data('test/tradingbot/test_data.csv')
+    mocker.patch('src.tradingbot.tradingbot.load_ticker_data', return_value=df)
+    s = TradingbotSolution(chromosome=np.asarray([]), account_balance=20)
+    timestamp_bought = '1970-01-01'
+    timestamp_sold = '1970-01-05'
+
+    s.buy(datetime=timestamp_bought, amount=10)
+    assert s.account_balance == 10
+
+    s.sell(datetime=timestamp_sold)  # Profit 5x
+
+    assert len(s.bought_positions) == 0
+    assert s.account_balance == 10 + 50
 
 
 @mockenv(TRADED_TICKER_NAME="AAPL", START_TIMESTAMP="34361", END_TIMESTAMP="811961", BUDGET="100", TRADE_SIZE="10")
@@ -63,7 +80,7 @@ def test_tradingbotsolution_fitness(mocker):
     mocker.patch('src.tradingbot.decisions.TradingStrategies.perform_decisions_for_row',
                  return_value={"dummy_strategy": Decision.BUY})
     result: float = fitness(chromosome)
-    np.testing.assert_almost_equal(result, 192.89, decimal=2)
+    np.testing.assert_almost_equal(result, 292.89, decimal=2)
 
     # Decisions: Inconclusive every day
     mocker.patch('src.tradingbot.decisions.TradingStrategies.perform_decisions_for_row',
@@ -81,4 +98,4 @@ def test_tradingbotsolution_fitness(mocker):
     mocker.patch('src.tradingbot.decisions.TradingStrategies.perform_decisions_for_row',
                  side_effect=[{"dummy_strategy": Decision.BUY if i < 5 else Decision.SELL} for i in range(10)])
     result: float = fitness(chromosome)
-    np.testing.assert_almost_equal(result, 164.16, decimal=2)
+    np.testing.assert_almost_equal(result, 214.16, decimal=2)