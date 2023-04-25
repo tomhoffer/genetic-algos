67d6b652d255ae1fa4178167c926d4f43640a407
Tomas Hoffer <tomas.hoffer@gmail.com>
2023-04-25 19:08:21 +0200

    Tradingbot: Fix missing backtesting options in config

--- src/tradingbot/config.py
+++ src/tradingbot/config.py
@@ -18,6 +18,8 @@ class Config:
     TRADE_SIZE: float
     START_TIMESTAMP: int
     END_TIMESTAMP: int
+    BACKTEST_END_TIMESTAMP: int
+    BACKTEST_START_TIMESTAMP: int
     MAX_ITERS: int
     N_PROCESSES: int
     POPULATION_SIZE: int
