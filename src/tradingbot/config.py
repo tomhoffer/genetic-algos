298bd24ef8be53652019574833b9e0c4fd836c78
Tomas Hoffer <tomas.hoffer@gmail.com>
2023-04-25 16:59:25 +0200

    Revert "Tradingbot: Add backtest periods to the config"
    This reverts commit cacef410970d16c094222ee1d955dd25746c53c5.

--- src/tradingbot/config.py
+++ src/tradingbot/config.py
@@ -18,8 +18,6 @@ class Config:
     TRADE_SIZE: float
     START_TIMESTAMP: int
     END_TIMESTAMP: int
-    BACKTEST_START_TIMESTAMP: int
-    BACKTEST_END_TIMESTAMP: int
     MAX_ITERS: int
     N_PROCESSES: int
     POPULATION_SIZE: int
